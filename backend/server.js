5a2d264c77a599f1d3683d97f19ceb4fc2fcdd18
Diego <diego.buitrago@hass-colombia.com>
2025-03-03 12:41:52 -0500

    configuración para utilizar frontend desde backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const morgan = require('morgan');
 const express = require('express');
 require('dotenv').confi
 const cors = require('cors');
+const path = require("path");
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
@@ -9,6 +10,15 @@ app.set('port', process.env.PORT || 5000);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
+
+
+// Sirve archivos estáticos de React
+app.use("/", express.static(path.join(__dirname, "../frontend/dist")));
+
+// Maneja todas las rutas restantes devolviendo el index.html
+app.get('*', (_req, res) => {
+   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 // Importar rutas
 app.use("/api", require('./src/routes/eventos.Route'));
--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -2,18 +2,6 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST || "",
-//     user: process.env.DB_USER || "",
-//     password: process.env.DB_PASS || "",
-//     database: process.env.DB_DATABASE || "",
-//     port: process.env.DB_PORT || "",
-//     waitForConnections: true,
-//     connectionLimit: 20,
-//     queueLimit: 0,
-//     dateStrings: true,
-// });
-
 const pool = new Pool({
     user: process.env.DB_USER || "",
     host: process.env.DB_HOST || "",
